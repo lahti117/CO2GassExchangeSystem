81c71834fca84c0900efa3c69a56f6050affdf9f
Sauel Lahti <samuel.lahti@gmail.com>
2022-02-09 10:07:45 -0700

    Began working on the GUI

--- RaspberryPi/GUI.py
+++ RaspberryPi/GUI.py
@@ -2,3 +2,12 @@
 # and all the buttons and fields in the GUI
 
 import tkinter as tk
+
+window = tk.Tk()
+
+frame = tk.Frame()
+
+mainLabel = tk.Label(text="CO2 H20 Gas Exchange System Datalogger", master=frame, width=100, height=3)
+mainLabel.pack()
+frame.pack()
+window.mainloop()
