8747af6f0ba3e120fd57f5309b5940824594d983
Samuel Lathi <samuel.lahti@gmail.com>
2022-03-25 10:28:57 -0600

    Got interrupts set up

--- Arduino/interrupts/interrupts.ino
+++ Arduino/interrupts/interrupts.ino
@@ -1,22 +1,33 @@
 #include <stdint.h>
-unsigned int reload = 0xF424; 
+
+#define LEDPIN 13
+
+static bool output;
+unsigned int reload = 0xC35; 
 // https://microcontrollerslab.com/arduino-timer-interrupts-tutorial/
 ISR(TIMER1_COMPA_vect){
-  
+  if (output) {
+    digitalWrite(LEDPIN, LOW);
+  } else {
+    digitalWrite(LEDPIN, HIGH);
+  }
+  output = !output;
+  Serial.println(output);
 }
 
 void setup() {
     Serial.begin(9600);
-    cli();
+    pinMode(LEDPIN, OUTPUT);
+    digitalWrite(LEDPIN, LOW);
+    output = false;
     TCCR1A = 0;
     TCCR1B = 0; 
     OCR1A = reload;
     TCCR1B = (1<<WGM12) | (1<<CS12); 
     TIMSK1 = (1<<OCIE1A); 
-    sei(); 
     Serial.println("TIMER1 Setup Finished.");
 }
 
 void loop() {
-    
+    while(1);
 }
